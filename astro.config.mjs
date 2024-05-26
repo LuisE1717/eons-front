032c2f08a8023c8cdf7daca952e62e6b3d3eaa42
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-05-26 18:25:14 -0400

    feat: :art: A lot of ads

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output:'server',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   i18n: {
     defaultLocale: "en",
