2b7b75939067f5551082f5dcf3ec4fbd8e2effae
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-06-08 13:59:19 -0400

    Refactor email verification page

--- src/modules/EmailConfirmation/components/Header/Header.tsx
+++ src/modules/EmailConfirmation/components/Header/Header.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function Header() {
   return (
     <header className="flex justify-center w-full mb-12">
       <img
-        src="/email-verification.svg"
+        src="/pixelcut-export.webp"
         alt="email-verification-image"
         className="sm:w-[270px] w-[220px]"
       />
--- src/modules/EmailConfirmation/components/Message/Message.tsx
+++ src/modules/EmailConfirmation/components/Message/Message.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function Message() {
   return (
-    <div className="flex flex-col text-center mb-3.5">
-      <h1 className="mb-2 sm:text-3xl text-2xl font-semibold">
+    <div className="flex flex-col text-center mb-8">
+      <h1 className="mb-4 sm:text-2xl text-xl font-medium">
         Verificar tu cuenta de correo
       </h1>
-      <p className="text-gray-400 sm:text-lg text-base">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
-        eveniet officia aut quae neque mollitia dicta amet corrupti doloremque
-        ipsa.
+      <p className="text-gray-600 font-thin sm:text-base text-sm">
+        Ingresa correctamente el código asociado a tu cuenta. Pronto recibirás
+        un correo con la clave para restablecer tu contraseña
       </p>
     </div>
   );
