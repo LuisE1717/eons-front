819df31124a83584f6956fda1ffd07fe223a00bb
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-06-11 02:39:39 -0400

    Add spiritual family page

---
+++ src/modules/SpiritualFamily/components/Content/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import useTranslation from "../../../../../Shared/hooks/useTranslation";
+
+export default function Header() {
+  const { translation } = useTranslation();
+
+  return (
+    <header>
+      <h1 className="sm:text-3xl text-2xl font-semibold mb-5 text-center">
+        {translation.Spiritual_Family.header}
+      </h1>
+    </header>
+  );
+}
