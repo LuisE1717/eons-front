1fe82ecdd411c2470ae3ce2baace14f5018d66fc
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-06-13 15:17:13 -0400

    feat: :art: Service Menu

--- src/modules/Services/components/Second/components/MessageInfo/MessageInfo.tsx
+++ src/modules/Services/components/Second/components/MessageInfo/MessageInfo.tsx
@@ -1,34 +1,45 @@
+import useTranslation from "../../../../../Shared/hooks/useTranslation";
 import Line from "../../../../shared/components/Line/Line";
 import P from "../../../../shared/components/P/P";
 import Span from "../../../../shared/components/Span/Span";
 import Info from "../../shared/components/Info/Info";
 
 export default function MessageInfo() {
+  const { translation } = useTranslation();
   return (
     <Info>
       <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
+        {translation.ServiceMenu.general_evaluation} <Span>{translation.ServiceMenu.free}</Span>
       </P>
       <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
-      </P>
-      <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
-      </P>
-      <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
-      </P>
-      <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
+        {translation.ServiceMenu.spirit_calc} <Span>{translation.ServiceMenu.free}</Span>
       </P>
 
       <Line />
 
       <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
+        {translation.ServiceMenu.open_dialog_mod} <Span>{translation.ServiceMenu.free}</Span>
       </P>
       <P>
-        Lorem ipsum dolor sit amet. <Span>Precio</Span>
+        {translation.ServiceMenu.contact_spirit} <Span>{translation.ServiceMenu.free}</Span>
+      </P>
+
+      <Line />
+
+      <P>
+        {translation.ServiceMenu.day_dialog} <Span>{translation.ServiceMenu.free}</Span>
+      </P>
+      <P>
+        {translation.ServiceMenu.adivination_general} <Span>{translation.ServiceMenu.free}</Span>
+      </P>
+
+      <Line />
+
+      <P>
+        {translation.ServiceMenu.concient_evaluation} <Span>{translation.ServiceMenu.free}</Span>
+      </P>
+      <P>
+        {translation.ServiceMenu.fact_evaluation} <Span>{translation.ServiceMenu.free}</Span>
       </P>
     </Info>
   );
