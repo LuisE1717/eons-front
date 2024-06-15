d72a102cd53918d44550d0a605afe2a29fa4e46d
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-06-15 17:57:42 -0400

    Refactor services page

---
+++ src/modules/Services/components/Second/components/Empty/Empty.tsx
@@ -0,0 +1,11 @@
+export default function Empty() {
+  return (
+    <figure>
+      <img
+        src="./triangle.webp"
+        alt=""
+        className="object-contain w-full max-w-[500px]"
+      />
+    </figure>
+  );
+}
