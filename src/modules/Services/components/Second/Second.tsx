cd7505a59e4d16f21632be4502824badb7c2fdde
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-06-21 00:19:23 -0400

    Add services animation

--- src/modules/Services/components/Second/Second.tsx
+++ src/modules/Services/components/Second/Second.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { createRef, useState } from "react";
 import { SECTIONS } from "./constants";
 import BookInfo from "./components/BookInfo/BookInfo";
 import MessageInfo from "./components/MessageInfo/MessageInfo";
@@ -8,42 +8,56 @@ import User from "./components/User/User";
 import Book from "./components/Book/Book";
 import Message from "./components/Message/Message";
 import Empty from "./components/Empty/Empty";
+import { useInView, motion } from "framer-motion";
 
 export default function Second() {
+  const ref = createRef<HTMLDivElement>();
+
   const [selected, setSelected] = useState<SECTIONS | null>(null);
+
+  const isInView = useInView(ref, {
+    once: true,
+    amount: "all",
+  });
 
   function handleChange(s: SECTIONS) {
     setSelected(s);
   }
 
   return (
-    <section className="flex flex-col items-center sm:py-14 py-6 w-full">
-      <div className="flex justify-center mb-6 gap-x-10">
+    <section
+      ref={ref}
+      className="flex flex-col items-center sm:py-14 py-6 w-full"
+    >
+      <div ref={ref} className="flex justify-center mb-6 gap-x-10">
         <Icon
+          visible={isInView}
           selected={selected === SECTIONS.USER}
           icon={User}
           handleClick={() => handleChange(SECTIONS.USER)}
         />
       </div>
 
-      <div className="flex items-end sm:gap-x-12 gap-x-4 w-full justify-center">
+      <motion.div className="flex items-end sm:gap-x-12 gap-x-4 w-full justify-center">
         <Icon
+          visible={isInView}
           selected={selected === SECTIONS.BOOK}
           icon={Book}
           handleClick={() => handleChange(SECTIONS.BOOK)}
         />
 
-        {selected === null && <Empty />}
+        {selected === null && <Empty visible={isInView} />}
         {selected === SECTIONS.BOOK && <BookInfo />}
         {selected === SECTIONS.MESSAGES && <MessageInfo />}
         {selected === SECTIONS.USER && <UserInfo />}
 
         <Icon
+          visible={isInView}
           selected={selected === SECTIONS.MESSAGES}
           icon={Message}
           handleClick={() => handleChange(SECTIONS.MESSAGES)}
         />
-      </div>
+      </motion.div>
     </section>
   );
 }
--- src/modules/Services/components/Second/components/Empty/Empty.tsx
+++ src/modules/Services/components/Second/components/Empty/Empty.tsx
@@ -1,11 +1,20 @@
-export default function Empty() {
+import { motion } from "framer-motion";
+
+interface Props {
+  visible: boolean;
+}
+
+export default function Empty({ visible }: Props) {
   return (
-    <figure>
-      <img
-        src="/triangle.webp"
-        alt=""
-        className="object-contain w-full max-w-[500px]"
-      />
-    </figure>
+    <motion.figure
+      style={{
+        width: "100%",
+        maxWidth: `500px`,
+        transform: visible ? "scale(1)" : "scale(0.1)",
+      }}
+      className="transition-all duration-700"
+    >
+      <img src="/triangle.webp" alt="" className="object-contain w-full" />
+    </motion.figure>
   );
 }
