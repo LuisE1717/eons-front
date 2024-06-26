7d5002a45ba220bae4ce206ef45022787fab6bd2
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-06-26 08:07:31 -0400

    feat: :art: Language Feature Success

--- src/modules/Shared/hooks/useTranslation.tsx
+++ src/modules/Shared/hooks/useTranslation.tsx
@@ -3,10 +3,12 @@ import { getI18N } from "../../../i18n";
 import { LanguageContext } from "../context/LanguageContext";
 import { useStore } from '@nanostores/react';
 import { locale } from '../../../LanguageStore';
+import Cookies from "js-cookie";
 
 export default function useTranslation() {
   const $locale = useStore(locale)
-  const translation = getI18N({ currentLocale: $locale});
+
+  const translation = getI18N(Cookies.get("eons_lng")? {currentLocale:Cookies.get("eons_lng")||"en"} :{ currentLocale: $locale});
 
   return { translation };
 }
