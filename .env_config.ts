efa1eec3ae6b6c3de07804b155ea1a41dbf47f95
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-07-03 04:01:03 -0400

    fix: :zap: Ready

---
+++ .env_config.ts
@@ -0,0 +1,20 @@
+const env = 'production';
+//const env = 'production';
+//const env = 'refactor';
+
+const configEnv = {
+  development: {
+    api: 'https://eons-back.onrender.com/',
+    env: 'development'
+  },
+  production: {
+    api: 'https://eons-back.onrender.com/',
+    env: 'production'
+  },
+  local: {
+    api: 'http://localhost:3000/',
+    env: 'local'
+  }
+}[env];
+
+export default configEnv;
