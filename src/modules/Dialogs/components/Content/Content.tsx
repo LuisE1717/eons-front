a197df56ad6f1c4fca75def848b58c257c0d2679
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-07-14 14:21:48 -0400

    Refactor dialogs page

--- src/modules/Dialogs/components/Content/Content.tsx
+++ src/modules/Dialogs/components/Content/Content.tsx
@@ -1,40 +1,49 @@
-import Button from "../../../../components/UI/Button/Button";
-import ListContainer from "../../../Shared/components/ListContainer/ListContainer";
 import { SECTIONS } from "../../constants";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Favorites from "./components/Favorites/Favorites";
+import Image from "./components/Image/Image";
 import Switch from "./components/Switch/Switch";
 import useContent from "./hooks/useContent";
 
-export default function Content({type}) {
-  const { selected, handleChangeSection, handleAddDialog, handleDeleteDialog, dataDialogs } =
-    useContent(type);
+export default function Content({ type }) {
+  const {
+    selected,
+    handleChangeSection,
+    handleAddDialog,
+    handleDeleteDialog,
+    dataDialogs,
+  } = useContent(type);
 
   return (
-  <>
-    <ListContainer>
-      <Switch handleChangeSection={handleChangeSection} selected={selected} />
+    <main className="flex flex-col items-center px-6 pb-20">
+      <div className="w-full max-w-[1100px] flex flex-col">
+        <div className="flex lg:flex-row flex-col-reverse items-center justify-between w-full gap-x-10 gap-y-10">
+          <div className="flex flex-col items-center lg:items-start w-full max-w-[500px]">
+            <Switch
+              handleChangeSection={handleChangeSection}
+              selected={selected}
+            />
 
-      {selected === SECTIONS.DIALOGS && (
-        <Dialogs
-          dialogs={dataDialogs.data}
-          handleAddDialog={handleAddDialog}
-          handleDeleteDialog={handleDeleteDialog}
-        />
-      )}
-      {selected === SECTIONS.FAVORITES && (
-        <Favorites
-          dialogs={dataDialogs.data}
-          handleAddDialog={handleAddDialog}
-          handleDeleteDialog={handleDeleteDialog}
-        />
-      )}
-    </ListContainer>
-    <div className="flex flex-col w-full items-center gap-x-3 mb-4">
-      <a href="/services">
-        <Button loading={false}>{"Volver al Menu"}</Button>
-      </a>
-    </div>
-  </>
+            {selected === SECTIONS.DIALOGS && (
+              <Dialogs
+                dialogs={dataDialogs}
+                handleAddDialog={handleAddDialog}
+                handleDeleteDialog={handleDeleteDialog}
+              />
+            )}
+
+            {selected === SECTIONS.FAVORITES && (
+              <Favorites
+                dialogs={dataDialogs}
+                handleAddDialog={handleAddDialog}
+                handleDeleteDialog={handleDeleteDialog}
+              />
+            )}
+          </div>
+
+          <Image />
+        </div>
+      </div>
+    </main>
   );
 }
--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,64 +1,65 @@
-import axios, {Axios, type AxiosInstance, type InternalAxiosRequestConfig } from "axios";
+import axios, {
+  Axios,
+  type AxiosInstance,
+  type InternalAxiosRequestConfig,
+} from "axios";
 import configEnv from "../../../.env_config";
 import Cookies from "js-cookie";
 import { setCookie } from "../cookies/Cookies";
 import { validMail } from "../validations";
 import { userProfile } from "../../UserStore";
 
-export const intanceAxios : AxiosInstance = axios.create ({
-    baseURL: configEnv?.api
+export const intanceAxios: AxiosInstance = axios.create({
+  baseURL: configEnv?.api,
 });
 
-export function axiosI(apiToken : string|undefined ) {
-    //console.log(configEnv)
+export function axiosI(apiToken: string | undefined) {
+  //console.log(configEnv)
 
-    const intance = axios.create({
-        baseURL: configEnv?.api
-    });
+  const intance = axios.create({
+    baseURL: configEnv?.api,
+  });
 
-    intance.interceptors.request.use(
-        (config: InternalAxiosRequestConfig) => {
-            if (!apiToken) 
-            {
-                apiToken = Cookies.get("eons_token") || '';
-            }
+  intance.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      if (!apiToken) {
+        apiToken = Cookies.get("eons_token") || "";
+      }
 
+      if (apiToken && config.headers) {
+        config.headers.Authorization = `Bearer ${apiToken}`;
+      }
 
-            if(apiToken && config.headers)
-            {
-                config.headers.Authorization = `Bearer ${apiToken}`;
-            }
-       
-            //console.log("Request was sent", config);
-            return config;
-        },
-        (error) => {
-            return Promise.reject(error);
+      //console.log("Request was sent", config);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  intance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.response) {
+        const originalConfig = error.config;
+        // Access Token was expired
+        setCookie("comeback_url", window.location.href, 0.25);
+        if (error.response.status === 401) {
+          window.location.reload();
+        } else if (error.response.status === 403) {
+          if (validMail(Cookies.get("eons_user")))
+            window.location.href = `/auth/email-verification/${
+              Cookies.get("eons_user") || ""
+            }`;
+          else window.location.href = `/auth`;
         }
-    );
+      }
+      return Promise.reject(error);
+    }
+  );
 
-    intance.interceptors.response.use(
-        (response) => {
-            return response;
-        },
-        async (error) => {
-            if (error.response) {
-                const originalConfig = error.config;
-                // Access Token was expired
-                setCookie('comeback_url',window.location.href,0.25)
-                if (error.response.status === 401) {
-                    window.location.reload()
-                }
-                else if (error.response.status === 403){
-                    if(validMail(Cookies.get('eons_user')))
-                        window.location.href =`/auth/email-verification/${Cookies.get('eons_user') || ''}`
-                    else
-                        window.location.href =`/auth` 
-                }
-            }
-            return Promise.reject(error);
-        }
-    );
-
-    return intance;
+  return intance;
 }
