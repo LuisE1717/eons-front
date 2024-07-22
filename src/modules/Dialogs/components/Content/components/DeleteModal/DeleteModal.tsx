4c97a3ee9b9120efb5d21d9355bdd7467d49a87e
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-07-22 13:41:35 -0400

    Refactor delete modal

--- src/modules/Dialogs/components/Content/components/DeleteModal/DeleteModal.tsx
+++ src/modules/Dialogs/components/Content/components/DeleteModal/DeleteModal.tsx
@@ -17,11 +17,28 @@ export default function DeleteModal({
 
   return (
     <Modal open={open} onClose={handleClose} center showCloseIcon={false}>
-      <h2 className="text-xl font-semibold">Eliminar diálogo</h2>
-      <p>¿Está seguro que desea eliminar el diálogo? </p>
+      <h2 className="text-xl font-semibold mb-4">Eliminar diálogo</h2>
 
-      <div className="flex justify-end w-full mt-3">
-        <Button loading={false} onClick={handleSubmit} full={false} size="sm">
+      <p>
+        ¿Está seguro que desea eliminar el diálogo? Escriba{" "}
+        <span>Eliminar</span> en el cuadro de texto
+      </p>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="outline-none w-full border-b-2 mt-2.5 border-gray-300 pb-1.5 focus:border-gray-400 transition-all duration-200"
+      />
+
+      <div className="flex justify-end w-full mt-4">
+        <Button
+          disabled={text !== "Eliminar"}
+          loading={false}
+          onClick={handleSubmit}
+          full={false}
+          size="sm"
+        >
           Eliminar
         </Button>
       </div>
