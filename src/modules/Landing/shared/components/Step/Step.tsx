6d5677ae7b42019f81ced1e67c3d924af377e3b5
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-07-26 13:39:08 -0400

    feat: :art: styless

--- src/modules/Landing/shared/components/Step/Step.tsx
+++ src/modules/Landing/shared/components/Step/Step.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Step({ children }: Props) {
   return (
-    <div className="text-center flex w-full lg:pt-14 lg:pb-14 pb-14 pt-4 text-lg px-14">
+    <div className="text-start flex w-full lg:pt-14 lg:pb-14 pb-14 pt-4 text-lg">
       {children}
     </div>
   );
