c15acbc507ef2a646ca88c67bbefb99138917ba7
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-07-28 15:19:14 -0400

    feat: :art: changes

--- src/modules/Services/components/Second/components/BookInfo/BookInfo.tsx
+++ src/modules/Services/components/Second/components/BookInfo/BookInfo.tsx
@@ -1,18 +1,27 @@
 import useTranslation from "../../../../../Shared/hooks/useTranslation";
+import A from "../../../../shared/components/A/A";
+import Blink from "../../../../shared/components/Blink/Blink";
 import Line from "../../../../shared/components/Line/Line";
 import P from "../../../../shared/components/P/P";
 import Span from "../../../../shared/components/Span/Span";
 import Info from "../../shared/components/Info/Info";
 
-export default function BookInfo() {
+export default function BookInfo({first_time}) {
   const { translation } = useTranslation();
 
   return (
     <Info>
+      {first_time?
+      <Blink>
+        {translation.ServiceMenu.use_instruction}{" "}
+        <Span>{translation.ServiceMenu.free}</Span>
+      </Blink>
+      :
       <P>
         {translation.ServiceMenu.use_instruction}{" "}
         <Span>{translation.ServiceMenu.free}</Span>
       </P>
+      }
       <Line />
       <P>
         {translation.ServiceMenu.thinks_to_know}{" "}
