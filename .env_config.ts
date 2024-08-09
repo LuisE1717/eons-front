b18d6aca389df0555cbc40d727e6b86b41d021d7
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-08-09 01:50:39 -0400

    Add border to throw card

--- .env_config.ts
+++ .env_config.ts
@@ -1,20 +1,20 @@
-const env = 'local';
+const env = "production";
 //const env = 'production';
 //const env = 'refactor';
 
 const configEnv = {
   development: {
-    api: 'https://eons-back.onrender.com/',
-    env: 'development'
+    api: "https://eons-back.onrender.com/",
+    env: "development",
   },
   production: {
-    api: 'https://eons-services.onrender.com/',
-    env: 'production'
+    api: "https://eons-services.onrender.com/",
+    env: "production",
   },
   local: {
-    api: 'http://localhost:3000/',
-    env: 'local'
-  }
+    api: "http://localhost:3000/",
+    env: "local",
+  },
 }[env];
 
 export default configEnv;
