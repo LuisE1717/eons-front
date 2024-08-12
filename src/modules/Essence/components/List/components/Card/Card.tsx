ab586d1fc44102450ec854d5e7e2d242e3bea0d0
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-08-12 19:31:05 -0400

    feat: :art: fehdfh

--- src/modules/Essence/components/List/components/Card/Card.tsx
+++ src/modules/Essence/components/List/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "../../../../../Shared/hooks/useTranslation";
 import type { Essence } from "../../domain";
 import Fire from "./components/Fire/Fire";
 
@@ -6,7 +7,33 @@ interface Props {
   handleClick(): void;
 }
 
+function separarTexto(texto:string) {
+  // Expresión regular para encontrar un número al inicio del texto
+  // y luego capturar las palabras "de" y "Esencia"
+  const regex = /^(\d+)(\sde\sEsencia)?$/;
+
+  // Utilizamos el método match() para buscar coincidencias
+  const resultado = texto.match(regex);
+
+  // Si encontramos una coincidencia, extraemos las partes
+  if (resultado && resultado.length > 1) {
+    // El primer grupo captura el número
+    const numero = resultado[1];
+    // El segundo grupo captura las palabras "de" y "Esencia", si están presentes
+    const resto = resultado[2] ? 'de Esencia' : '';
+
+    return {
+      numero,
+      resto,
+    };
+  }
+
+  // Si no se encuentra ninguna coincidencia, retornamos null o un mensaje de error
+  return null;
+}
+
 export default function Card({ essence, handleClick }: Props) {
+  const {translation} = useTranslation()
   return (
     <div onClick={handleClick}>
     <article
@@ -16,7 +43,7 @@ export default function Card({ essence, handleClick }: Props) {
         <Fire />
       </figure>
 
-      <h2 className="font-semibold text-base mb-0.5">{essence.descripcion}.</h2>
+      <h2 className="font-semibold text-base mb-0.5">{separarTexto(essence.descripcion)?.numero + " " + translation.of_esence}.</h2>
 
       <span className="text-sm text-gray-400">
         {`${essence.precio}€`}{" "}
