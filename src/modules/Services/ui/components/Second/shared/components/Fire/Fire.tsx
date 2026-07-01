30725067b4eda60d8dd08f63b4b0bfab765ddbab
Alejandro Roble Gutiérrez <arobleg@gmail.com>
2026-07-01 13:30:51 +0200

    chore: remove dead files
    Delete 52 unreferenced files identified by knip and verified unimported
    (build + astro check + tests all green):
    - dead SpiritualFamily Content/* subtree (superseded by NewContent)
    - unused Notifications (Mail/Option/Select), Services and payment leftovers
    - orphaned language-flag assets, root types.ts, dead utils and components

--- src/modules/Services/ui/components/Second/shared/components/Fire/Fire.tsx
+++
@@ -1,3 +0,0 @@
-export default function Fire() {
-    return <img src="/fire.webp" alt="" className="object-contain w-[30px]" />;
-}
--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface CoinPosition {
-    isFaceUp: boolean;
-    isOuterCircleFilled: boolean
-}
